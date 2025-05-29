75cab974e63f0a41ae24f3aeae2ede1cd30742c2
Jose <jzerpa1@udi.edu.co>
2025-05-29 10:07:36 -0500

    index

--- api/index.js
+++
@@ -1,4 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../src/app');  // Importas tu app.js
-
-module.exports = serverless(app);  // Lo adaptas al entorno serverless
---
+++ src/index.js
@@ -0,0 +1,7 @@
+// index-local.js
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
