dd904205b0c342c85043ac68c51126d04a610fab
Jose <jzerpa1@udi.edu.co>
2025-05-28 19:14:45 -0500

    cambio del index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const serverless = require('serverless-http');
-const app = require('.././src/app');  // Importas tu app.js
+const app = require('./src/app');  // Importas tu app.js
 
 module.exports = serverless(app);  // Lo adaptas al entorno serverless
